2763d5049a2f47668186c25fea9b9691e46a1119
aritroCoder <ogcodinggod@gmail.com>
2022-12-02 21:17:12 +0530

    hardhat simple storage

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,20 +1,40 @@
-require("@nomicfoundation/hardhat-toolbox");
-require("@nomiclabs/hardhat-etherscan");
-require("dotenv").config()
+require('@nomicfoundation/hardhat-toolbox')
+require('@nomiclabs/hardhat-etherscan')
+require('dotenv').config()
+require('./tasks/block-number')
+require("hardhat-gas-reporter")
+require('solidity-coverage')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const goerliRPCUrl = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const goerliRPCUrl = process.env.GOERLI_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "key"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "key"
 
 module.exports = {
-  defaultNetwork: "hardhat",
-  networks: {
-    goerli:{
-      url: goerliRPCUrl,
-      accounts:[PRIVATE_KEY],
-      chainId: 5,
-    }
-  },
-  solidity: "0.8.8",
-};
+    defaultNetwork: 'hardhat',
+    networks: {
+        // add additional networks here
+        goerli: {
+            url: goerliRPCUrl,
+            accounts: [PRIVATE_KEY],
+            chainId: 5, // chainId for goerli = 5
+        },
+        localhost: {
+            url: 'http://127.0.0.1:8545/',
+            chainId: 31337, // same chaid as hardhat
+        },
+    },
+    solidity: '0.8.8',
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: 'gas-report.txt', // optional: Output report to a file
+        noColors: true,
+        currency: 'USD',
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,52 @@
 // imports
-const { ethers } = require("hardhat");
+const { ethers, run, network } = require('hardhat')
 
 //async main
 async function main() {
-  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
-  console.log("Deploying SimpleStorage...");
-  const simpleStorage = await SimpleStorageFactory.deploy();
-  await simpleStorage.deployed();
-  console.log(`deployed to: ${simpleStorage.address}`);
-}
-//by default it gets deployed to hardhat default network
+  const SimpleStorageFactory = await ethers.getContractFactory(
+    'SimpleStorage'
+  )
+  console.log('Deploying SimpleStorage...')
+  const simpleStorage = await SimpleStorageFactory.deploy()
+  await simpleStorage.deployed() // wait till contract is deployed
+  console.log(`deployed to: ${simpleStorage.address}`) 
+  if(network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY){ // goerli network
+    await simpleStorage.deployTransaction.wait(6) // wait for 6 blocks
+    await verify(simpleStorage.address, []);
+  }
 
-async function verify(contractAddress, args){
-  
+  // interacting with contract
+  const currentValue = await simpleStorage.retrieve()
+  console.log("Current value: " + currentValue)
+  const transactionResponse = await simpleStorage.store(7);
+  await transactionResponse.wait(1); // wait for 1 transaction
+  const updatedValue = await simpleStorage.retrieve();
+  console.log("updatedValue: " + updatedValue)
+}
+//by default it gets deployed to hardhat default network if no args is passed
+
+async function verify(contractAddress, args) {
+  // verify contract code on etherscan
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args
+    })
+
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified")
+    } else {
+      console.log(error)
+    }
+  }
 }
 
 //call main
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  }
-);
+    console.error(error)
+    process.exit(1)
+  })
